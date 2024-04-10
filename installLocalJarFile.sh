ed8434d257f5fd70573d002f10e1b23605533915
Com4t <comfort@seedlegals.com>
2024-04-10 15:34:40 +0100

    init1

---
+++ installLocalJarFile.sh
@@ -0,0 +1,8 @@
+mvn install:install-file \
+   -Dfile=cssparser-0.9.30.jar \
+   -DgroupId=com.steadystate \
+   -DartifactId=css \
+   -Dversion=0.9.30 \
+   -Dpackaging=jar \
+   -DgeneratePom=true
+
